1279e9f91d3fc764fe5160a934b0181adec085fc
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-08 23:27:52 -0500

    Add files via upload

---
+++ 005_59.py
@@ -0,0 +1,36 @@
+class Solution:
+    def generateMatrix(self, n):
+        matrix = [[0] * n for i in range(n)]
+        
+        left = 0
+        right = n-1
+        up = 0
+        down = n-1
+        number = 1
+        
+        while left < right and up < down:
+            for x in range(left, right):
+                matrix[up][x] = number
+                number += 1
+            for y in range(up, down):
+                matrix[y][right] = number
+                number += 1
+            for x in range(right, left, -1):
+                matrix[down][x] = number
+                number += 1    
+            for y in range(down, up, -1):
+                matrix[y][left] = number
+                number += 1
+            
+            left += 1
+            right -= 1
+            up += 1
+            down -= 1
+        
+        if n % 2:
+            matrix[n//2][n//2] = number
+        
+        return matrix
+
+s1 = Solution()
+print(s1.generateMatrix(4))
