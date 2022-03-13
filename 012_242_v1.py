928c8d118a587ebb3046f31d126c7dcf53766f55
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-12 20:52:02 -0500

    Add files via upload

---
+++ 012_242_v1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s, t):
+        record = 26 * [0]
+        for i in range(len(s)):
+            record[ord(s[i])-ord('a')] += 1
+        for i in range(len(t)):
+            record[ord(t[i])-ord('a')] -= 1
+        for i in range(26):
+            if record[i] != 0:
+                return False
+                break
+        return True
+
+s = Solution()
+print(s.isAnagram('anagram', 'nagaram'))
+print(s.isAnagram('rat', 'car'))
