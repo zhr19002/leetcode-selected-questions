a743628e156a3336c17352825ef07d03ce9ef05f
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-14 01:26:08 -0400

    Add files via upload

---
+++ 018_383_v1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        record = 26 * [0]
+        for i in magazine:
+            record[ord(i) - ord('a')] += 1
+        for i in ransomNote:
+            if record[ord(i) - ord('a')]:
+                record[ord(i) - ord('a')] -= 1
+            else:
+                return False
+        return True
+
+s = Solution()
+print(s.canConstruct('a', 'b'))
+print(s.canConstruct('aa', 'ab'))
+print(s.canConstruct('aa', 'aab'))
