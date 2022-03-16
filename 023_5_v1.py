c79bee091e764b86c5d756620ce0e1bf23715ca3
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-16 00:53:20 -0400

    Add files via upload

---
+++ 023_5_v1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def replaceSpace(self, s):
+        counter = s.count(' ')
+        res = list(s)
+        res.extend([' ']*counter*2)
+        
+        left = len(s) - 1
+        right = len(res) - 1
+        
+        while left:
+            if res[left] != ' ':
+                res[right] = res[left]
+                right -= 1
+            else:
+                res[right-2 : right+1] = '%20'
+                right -= 3
+            left -= 1
+            
+        return ''.join(res)
+
+s = Solution()
+print(s.replaceSpace('We are happy.'))
