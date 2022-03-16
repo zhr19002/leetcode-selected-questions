32e527fbb9a63eb7202993e3d3fe0cc8c4b64997
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-16 00:53:09 -0400

    Add files via upload

---
+++ 022_541.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseStr(self, s, k):
+        p = 0
+        while p < len(s):
+            p2 = p + k
+            s = s[:p] + s[p:p2][::-1] + s[p2:]
+            p = p + 2*k
+        return s
+
+s = Solution()
+print(s.reverseStr('abcdefg', 2))
