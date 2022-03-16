e7c31a5c93902cfca1b41ad74d5bd239327527e5
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-15 23:45:46 -0400

    Add files via upload

---
+++ 021_344.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s):
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            tmp = s[left]
+            s[left] = s[right]
+            s[right] = tmp
+            left += 1
+            right -= 1
+        return s
+
+s = Solution()
+print(s.reverseString(['h','e','l','l','o']))
+print(s.reverseString(['H','a','n','n','a','h']))
