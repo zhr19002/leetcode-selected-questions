308d68a6dce5217af7cd0838532bda411a51d988
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-17 02:27:56 -0400

    Add files via upload

---
+++ 026_28.py
@@ -0,0 +1,36 @@
+def getNext(s):
+    nxt = ['' for i in range(len(s))]
+    nxt[0] = 0
+    j = 0
+    for i in range(1, len(s)):
+        while j > 0 and s[i] != s[j]:
+            j = nxt[j-1]
+        if s[i] == s[j]:
+            j += 1
+        nxt[i] = j
+    return nxt    
+
+class Solution:
+    def strStr(self, haystack, needle):
+        if len(needle) == 0:
+            return 0
+        
+        i = j = 0
+        nxt = getNext(needle)
+        nxt = [-1] + nxt[:-1]
+        while i < len(haystack) and j < len(needle):
+            if j == -1 or needle[j] == haystack[i]:
+                i += 1
+                j += 1
+            else:
+                j = nxt[j]
+        
+        if j == len(needle):
+            return i-j
+        else:
+            return -1
+        
+s = Solution()
+print(s.strStr('hello', 'll'))
+print(s.strStr('aaaaa', 'bba'))
+print(s.strStr('aabaacaabaaf', 'aabaaf'))
