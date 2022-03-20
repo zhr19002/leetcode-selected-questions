4d85ebb9435ed348a58be657e11e609c9d7c13a5
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-19 20:38:44 -0400

    Add files via upload

---
+++ 029_225.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+class MyStack:
+    def __init__(self):
+        self.queue_in = deque()
+        self.queue_out = deque()
+    
+    def push(self, x):
+        self.queue_in.append(x)
+    
+    def pop(self): #Remove the element from the queue and return that element
+        if self.empty():
+            return None
+        for i in range(len(self.queue_in)-1):
+            self.queue_out.append(self.queue_in.popleft())
+        tmp = self.queue_in
+        self.queue_in = self.queue_out
+        self.queue_out = tmp
+        return self.queue_out.popleft()
+        
+    def top(self): #Get the front element
+        if self.empty():
+            return None
+        return self.queue_in[-1]
+    
+    def empty(self):
+        if len(self.queue_in) != 0:
+            return False
+        else:
+            return True
+
+stack = MyStack()
+stack.push(1)
+stack.push(2)
+print(stack.pop())
+stack.push(3)
+stack.push(4)
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.empty())
