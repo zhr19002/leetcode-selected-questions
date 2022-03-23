c24be94ec416600e5d2a149bb82521dabcced216
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-23 02:05:44 -0400

    Add files via upload

---
+++ 034_347.py
@@ -0,0 +1,24 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        freq = {}
+        for i in range(len(nums)):
+            freq[nums[i]] = freq.get(nums[i],0) + 1
+        
+        pri_queue = []
+        for key,value in freq.items():
+            heapq.heappush(pri_queue, (value,key))
+            if len(pri_queue) > k:
+                heapq.heappop(pri_queue)
+            
+        result = [0] * k
+        for i in range(k-1, -1, -1):
+            result[i] = heapq.heappop(pri_queue)[1]
+        
+        return result
+
+s = Solution()
+print(s.topKFrequent([1,1,1,2,2,3], 2))
+print(s.topKFrequent([1], 1))
+print(s.topKFrequent(['a','a','a','a','b','b','b','c','c','d'], 3))
