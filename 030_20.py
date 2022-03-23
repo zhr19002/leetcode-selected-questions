82961433f087a6b1db651eab97f7dc9399de2a68
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-22 22:16:13 -0400

    Add files via upload

---
+++ 030_20.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        for item in s:
+            if item == '(':
+                stack.append(')')
+            elif item == '[':
+                stack.append(']')
+            elif item == '{':
+                stack.append('}')
+            elif not stack or stack[-1] != item:
+                return False
+            else:
+                stack.pop()
+        return True if not stack else False
+    
+s = Solution()
+print(s.isValid('()'))
+print(s.isValid('()[]{}'))
+print(s.isValid('(]'))
+print(s.isValid('([)]'))
+print(s.isValid('{[]}'))
