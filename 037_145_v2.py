9a07c0abf524bd4324d034e7d00e225a06bc4d47
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-25 00:09:08 -0400

    Add files via upload

---
+++ 037_145_v2.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def list2tree(lst):
+    root = TreeNode(lst[0])
+    root.left = TreeNode(lst[1])
+    root.right = TreeNode(lst[2])
+    root.left.left = TreeNode(lst[3])
+    root.left.right = TreeNode(lst[4])
+    root.right.left = TreeNode(lst[5])
+    root.right.right = TreeNode(lst[6])
+    return root
+
+class Solution:
+    def postorderTraversal(self, lst):
+        root = list2tree(lst)
+        if not root:
+            return []
+        stack = [root]
+        result = []
+        
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return result[::-1]
+
+s = Solution()
+print(s.postorderTraversal([5,4,6,1,2,7,8]))
