7162a083100efacdf38528d901a01393d635b4c9
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-25 00:08:58 -0400

    Add files via upload

---
+++ 036_94_v2.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def list2tree(lst):
+    root = TreeNode(lst[0])
+    root.left = TreeNode(lst[1])
+    root.right = TreeNode(lst[2])
+    root.left.left = TreeNode(lst[3])
+    root.left.right = TreeNode(lst[4])
+    root.right.left = TreeNode(lst[5])
+    root.right.right = TreeNode(lst[6])
+    return root
+
+class Solution:
+    def inorderTraversal(self, lst):
+        root = list2tree(lst)
+        if not root:
+            return []
+        stack = []
+        result = []
+        cur = root
+        
+        while cur or stack:
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                result.append(cur.val)
+                cur = cur.right
+
+        return result
+
+s = Solution()
+print(s.inorderTraversal([5,4,6,1,2,7,8]))
