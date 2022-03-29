c93ea5e5b3971152f52b1880487659e1cc428c03
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-03-29 00:07:42 -0400

    Add files via upload

---
+++ 039_107.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def list2tree(lst):
+    root = TreeNode(lst[0])
+    root.left = TreeNode(lst[1])
+    root.right = TreeNode(lst[2])
+    root.left.left = TreeNode(lst[3])
+    root.left.right = TreeNode(lst[4])
+    root.right.left = TreeNode(lst[5])
+    root.right.right = TreeNode(lst[6])
+    return root
+
+class Solution:
+    def levelOrderBottom(self, lst):
+        root = list2tree(lst)
+        results = []
+        if root == None:
+            return results
+        queue = deque([root])
+        while queue:
+            result = []
+            for i in range(len(queue)):
+                cur = queue.popleft()
+                result.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            results.append(result)
+        results.reverse()
+        return results
+
+s = Solution()
+print(s.levelOrderBottom([3,9,20,None,None,15,7]))
