100fc7c891c6250319156cc4214c38b13e25836d
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-04-13 23:46:52 -0400

    Add files via upload

---
+++ 053_257_v2.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def list2tree(lst):
+    if lst[0] != None: root = TreeNode(lst[0])  
+    if lst[1] != None: root.left = TreeNode(lst[1])
+    if lst[2] != None: root.right = TreeNode(lst[2])
+    if lst[3] != None: root.left.left = TreeNode(lst[3])
+    if lst[4] != None: root.left.right = TreeNode(lst[4])
+    if lst[5] != None: root.right.left = TreeNode(lst[5])
+    if lst[6] != None: root.right.right = TreeNode(lst[6])
+    return root
+
+class Solution:
+    def binaryTreePaths(self, lst):
+        root = list2tree(lst)
+        path = []
+        result = []
+        if not root:
+            return result
+        self.traversal(root, path, result)
+        return result
+        
+    def traversal(self, cur, path, result):
+        path.append(cur.val)
+        if not cur.left and not cur.right:
+            sPath = ''
+            for i in range(len(path)-1):
+                sPath += str(path[i])
+                sPath += '->'
+            sPath += str(path[len(path)-1])
+            result.append(sPath)
+            return
+        if cur.left:
+            self.traversal(cur.left, path, result)
+            path.pop()
+        if cur.right:
+            self.traversal(cur.right, path, result)
+            path.pop()
+
+s = Solution()
+print(s.binaryTreePaths([1,2,3,None,5,None,None]))
