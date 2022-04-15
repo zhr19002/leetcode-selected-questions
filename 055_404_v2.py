45a6b39777dade00fa5457ee8fcff4b6e52c0e82
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-04-14 23:18:28 -0400

    Add files via upload

---
+++ 055_404_v2.py
@@ -0,0 +1,37 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def list2tree(lst):
+    if lst[0] != None: root = TreeNode(lst[0])  
+    if lst[1] != None: root.left = TreeNode(lst[1])
+    if lst[2] != None: root.right = TreeNode(lst[2])
+    if lst[3] != None: root.left.left = TreeNode(lst[3])
+    if lst[4] != None: root.left.right = TreeNode(lst[4])
+    if lst[5] != None: root.right.left = TreeNode(lst[5])
+    if lst[6] != None: root.right.right = TreeNode(lst[6])
+    return root
+
+class Solution:
+    def sumOfLeftLeaves(self, lst):
+        root = list2tree(lst)
+        if not root:
+            return 0
+        stack = [root]
+        res = 0
+        
+        while stack:
+            node = stack.pop()
+            if node.left and not node.left.left and not node.left.right:
+                res += node.left.val
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        
+        return res
+
+s = Solution()
+print(s.sumOfLeftLeaves([3,9,20,None,None,15,7]))
