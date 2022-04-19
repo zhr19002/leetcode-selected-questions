71c1558e93b3ae90b0aed8d9616a27d61697a064
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-04-18 23:26:39 -0400

    Update 046_104.py

--- 046_104.py
+++ 046_104.py
@@ -7,11 +7,13 @@ class TreeNode:
         self.right = None
 
 def list2tree(lst):
-    root = TreeNode(lst[0])
-    root.left = TreeNode(lst[1])
-    root.right = TreeNode(lst[2])
-    root.right.left = TreeNode(lst[5])
-    root.right.right = TreeNode(lst[6])
+    if lst[0] != None: root = TreeNode(lst[0])  
+    if lst[1] != None: root.left = TreeNode(lst[1])
+    if lst[2] != None: root.right = TreeNode(lst[2])
+    if lst[3] != None: root.left.left = TreeNode(lst[3])
+    if lst[4] != None: root.left.right = TreeNode(lst[4])
+    if lst[5] != None: root.right.left = TreeNode(lst[5])
+    if lst[6] != None: root.right.right = TreeNode(lst[6])
     return root
 
 class Solution:
