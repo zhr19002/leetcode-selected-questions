67d692f46d48cc272bf4d2e2b320507dbd7d22af
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-05-10 22:54:25 -0400

    Add files via upload

---
+++ 069_701_v1.py
@@ -0,0 +1,54 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def list2tree(lst):
+    if lst[0] != None: root = TreeNode(lst[0])  
+    if lst[1] != None: root.left = TreeNode(lst[1])
+    if lst[2] != None: root.right = TreeNode(lst[2])
+    if lst[3] != None: root.left.left = TreeNode(lst[3])
+    if lst[4] != None: root.left.right = TreeNode(lst[4])
+    if lst[5] != None: root.right.left = TreeNode(lst[5])
+    if lst[6] != None: root.right.right = TreeNode(lst[6])
+    return root
+
+def tree2list(root):
+    lst = [None] * 7
+    if root: 
+        lst[0] = root.val
+        if root.left: 
+            lst[1] = root.left.val
+            if root.left.left: 
+                lst[3] = root.left.left.val
+            if root.left.right: 
+                lst[4] = root.left.right.val
+        if root.right: 
+            lst[2] = root.right.val
+            if root.right.left: 
+                lst[5] = root.right.left.val
+            if root.right.right: 
+                lst[6] = root.right.right.val
+    else:
+        return []
+    return lst
+
+class Solution:    
+    def insertIntoBST(self, lst, val):
+        root = list2tree(lst)
+        return self.insertBST(root, val)
+        
+    def insertBST(self, root, val):
+        if not root:
+            return TreeNode(val)
+        
+        if root.val > val:
+            root.left = self.insertBST(root.left, val)
+        if root.val < val:
+            root.right = self.insertBST(root.right, val)
+        
+        return root
+
+s = Solution()
+print(tree2list(s.insertIntoBST([4,2,7,1,3,None,None], 5)))
