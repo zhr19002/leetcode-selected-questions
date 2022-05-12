57b2f40d1fd8b11f696c513fe59a8bb5c60630fb
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-05-12 14:02:03 -0400

    Add files via upload

---
+++ 071_669.py
@@ -0,0 +1,56 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def list2tree(lst):
+    if lst[0] != None: root = TreeNode(lst[0])  
+    if lst[1] != None: root.left = TreeNode(lst[1])
+    if lst[2] != None: root.right = TreeNode(lst[2])
+    if lst[3] != None: root.left.left = TreeNode(lst[3])
+    if lst[4] != None: root.left.right = TreeNode(lst[4])
+    if lst[5] != None: root.right.left = TreeNode(lst[5])
+    if lst[6] != None: root.right.right = TreeNode(lst[6])
+    return root
+
+def tree2list(root):
+    lst = [None] * 7
+    if root: 
+        lst[0] = root.val
+        if root.left: 
+            lst[1] = root.left.val
+            if root.left.left: 
+                lst[3] = root.left.left.val
+            if root.left.right: 
+                lst[4] = root.left.right.val
+        if root.right: 
+            lst[2] = root.right.val
+            if root.right.left: 
+                lst[5] = root.right.left.val
+            if root.right.right: 
+                lst[6] = root.right.right.val
+    else:
+        return []
+    return lst
+
+class Solution:    
+    def trimBST(self, lst, low, high):
+        root = list2tree(lst)
+        return self.trim(root, low, high)
+        
+    def trim(self, root, low, high):
+        if not root:
+            return None
+        
+        if root.val < low:
+            return self.trim(root.right, low, high)
+        if root.val > high:
+            return self.trim(root.left, low, high)
+        if low <= root.val <= high:
+            root.left = self.trim(root.left, low, high)
+            root.right = self.trim(root.right, low, high)
+            return root
+
+s = Solution()
+print(tree2list(s.trimBST([5,3,6,2,4,None,7], 4, 7)))
