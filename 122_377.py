881fb4e3d0d4ff469ddc8f2fb46f9fd3a7dddfd9
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-07-05 15:37:23 -0400

    Add files via upload

---
+++ 122_377.py
@@ -0,0 +1,14 @@
+class Solution:   
+    def combinationSum4(self, nums, target):
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        
+        for i in range(1, target + 1):
+            for j in nums:
+                if i >= j:
+                    dp[i] += dp[i - j]
+        
+        return dp[-1]
+
+s = Solution()
+print(s.combinationSum4([1,2,3], 4))
