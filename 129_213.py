0a9dc137a3bba66616879a842ed4c0d411e76bc7
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-07-13 13:40:28 -0400

    Add files via upload

---
+++ 129_213.py
@@ -0,0 +1,22 @@
+class Solution:   
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        val1 = self.rob_list(nums[1:])
+        val2 = self.rob_list(nums[:-1])
+        return max(val1, val2)
+    
+    def rob_list(self, nums):
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        
+        return dp[-1]
+
+s = Solution()
+print(s.rob([2,3,2]))
+print(s.rob([1,2,3,1]))
+print(s.rob([0]))
