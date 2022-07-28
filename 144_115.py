c601946ec268b0e0ef4412d1379ba4b0c8b6a561
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-07-28 12:12:34 -0400

    Add files via upload

---
+++ 144_115.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDistinct(self, s, t):
+        dp = [[0] * (len(t)+1) for i in range(len(s)+1)]
+        for i in range(len(s)):
+            dp[i][0] = 1
+        for j in range(1, len(t)):
+            dp[0][j] = 0
+            
+        for i in range(1, len(s)+1):
+            for j in range(1, len(t)+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[-1][-1]
+
+s = Solution()
+print(s.numDistinct('rabbbit', 'rabbit'))
+print(s.numDistinct('babgbag', 'bag'))
