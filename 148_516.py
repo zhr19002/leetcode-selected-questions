f5b779949656cc15896fe308c379284499b38c5d
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-08-02 18:07:42 -0400

    Add files via upload

---
+++ 148_516.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindromeSubseq(self, s):
+        dp = [[0] * len(s) for i in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = 1
+        
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i+1, len(s)):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        
+        return dp[0][-1]
+
+s = Solution()
+print(s.longestPalindromeSubseq('bbbab'))
+print(s.longestPalindromeSubseq('cbbd'))
