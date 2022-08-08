02e4e7a5777d3bed400fe99701f825f79527801b
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-08-07 23:52:39 -0400

    Update 151_503.py

--- 151_503.py
+++ 151_503.py
@@ -3,7 +3,7 @@ class Solution:
         result = [-1] * len(nums)
         stack = []
         for i in range(len(nums)*2):
-            while len(stack) and nums[i%len(nums)] > nums[stack[-1]]:
+            while len(stack) != 0 and nums[i%len(nums)] > nums[stack[-1]]:
                 result[stack[-1]] = nums[i%len(nums)]
                 stack.pop()
             stack.append(i%len(nums))
