dd7f053d2760ec930790bb11cdc7d0c64ede92e3
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-08-23 17:31:15 -0400

    Update 014_349.py

--- 014_349.py
+++ 014_349.py
@@ -2,6 +2,7 @@ class Solution:
     def intersection(self, nums1, nums2):
         return list(set(nums1) & set(nums2))
 
-s = Solution()
-print(s.intersection([1,2,2,1], [2,2]))
-print(s.intersection([4,9,5], [9,4,9,8,4]))
+if __name__ == '__main__':
+    s = Solution()
+    print(s.intersection([1,2,2,1], [2,2]))
+    print(s.intersection([4,9,5], [9,4,9,8,4]))
