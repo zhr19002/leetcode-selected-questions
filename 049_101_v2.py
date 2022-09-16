a74bd1f5689782190ccbf45b01fb326400f5bb67
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2022-09-15 23:40:16 -0400

    Update 049_101_v2.py

--- 049_101_v2.py
+++ 049_101_v2.py
@@ -35,6 +35,7 @@ class Solution:
             stack.append(rightNode.left)
         return True
 
-s = Solution()
-print(s.isSymmetric([1,2,2,3,4,4,3]))
-print(s.isSymmetric([1,2,2,None,3,None,3]))
+if __name__ == '__main__':
+    s = Solution()
+    print(s.isSymmetric([1,2,2,3,4,4,3]))
+    print(s.isSymmetric([1,2,2,None,3,None,3]))
